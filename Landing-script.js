e5ddd78e476c2c76fea29feef97050c99242546e
Sandhya602 <sandhyabv31@gmail.com>
2025-10-05 19:47:08 +0530

    Update Landing-script.js

--- Landing-script.js
+++ Landing-script.js
@@ -1,1 +1,8 @@
+// Smooth scroll to Features section on CTA button click
+const ctaBtn = document.querySelector('.cta-btn');
+
+ctaBtn.addEventListener('click', () => {
+  const featuresSection = document.querySelector('.features');
+  featuresSection.scrollIntoView({ behavior: 'smooth' });
+});
 
